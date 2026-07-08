cd9b31991efc098724b32fe47d10d3aa67cd91f6
berkayzgn <berkayzgn3623@gmail.com>
2026-07-08 13:53:27 +0300

    useState ile yeni görev ekleme özelliği eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
+import { useState } from "react";
 import TaskCard from "./components/TaskCard";
 import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "Mülakata hazırlan", completed: false },
+    { id: 2, title: "Proje kurulumu", completed: true },
+    { id: 3, title: "Git öğren", completed: true },
+  ]);
+
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+  function handleAddTask() {
+    if (newTaskTitle.trim() === "") return;
+
+    const newTask = {
+      id: Date.now(),
+      title: newTaskTitle,
+      completed: false,
+    };
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle("");
+  }
+
   return (
     <main className="app">
       <h1>Görev Takip</h1>
+      <div className="add-task">
+        <input
+          type="text"
+          placeholder="Yeni görev yaz..."
+          value={newTaskTitle}
+          onChange={(e) => setNewTaskTitle(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Ekle</button>
+      </div>
       <div className="task-list">
-        <TaskCard title="Mülakata hazırlan" completed={false} />
-        <TaskCard title="Proje kurulumu" completed={true} />
-        <TaskCard title="Git öğren" completed={true} />
-        <TaskCard title="Rest API'yi öğren" completed={false} />
+        {tasks.map((task) => (
+          <TaskCard
+            key={task.id}
+            title={task.title}
+            completed={task.completed}
+          />
+        ))}
       </div>
     </main>
   );
